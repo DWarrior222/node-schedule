09645f75cf0da296e3868342bef6358783cc46d6
guanshuaijie <1845543324@qq.com>
2021-08-26 14:04:19 +0800

    time

--- src/deploy.js
+++ src/deploy.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 const schedule = require("node-schedule");
 
-schedule.scheduleJob({ hour: 10, minute: 10 }, () => {
+schedule.scheduleJob({ hour: 11, minute: 10 }, () => {
   console.log('启动任务：' + new Date());
   shell.exec(`open -a Terminal /usr/local/bin/login-docker/deployfebook.sh`);
 });
